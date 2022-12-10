f8848dda253c8ddcdfeb490bde843d36e1a1f72c
Hugo_Furtado <71742937+HugoFur@users.noreply.github.com>
2022-12-10 13:19:05 -0300

    feat(controller): adicionada função de criar e salvar livros no DB

--- controller/book.controller.js
+++ controller/book.controller.js
@@ -2,8 +2,33 @@ const db = require("../models");
 const Book = db.books;
 
 exports.create = (req, res) => {
+
+    // validação de requisições
+    if (!req.body.title) {
+      res.status(400).send({ message: "O conteudo da requisição não pode ser vazio" });
+      return;
+    }
   
-};
+    // Função para criação de novo Book
+    const book = new Book({
+      title: req.body.title,
+      author: req.body.author,
+      published: req.body.published ? req.body.published : false
+    });
+  
+    // função para salvar book no banco de dados
+    book
+      .save(book)
+      .then(data => {
+        res.send(data);
+      })
+      .catch(err => {
+        res.status(500).send({
+          message:
+            err.message || "Ocorreu um erro durante a criação do Livro"
+        });
+      });
+  };
 
 exports.findAll = (req, res) => {
   
